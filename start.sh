2edc99c1e5c05a54cdb59933f1a3675d15ef7f30
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-12-15 14:21:08 +0100

    fix: always subscribe to gateway

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 DB_PATH="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}" refinery migrate -e DB_PATH -p /migrations
-if [ -z ${HAS_GATEWAY+x} ]; then
-    subscribe
-fi
+subscribe
 application
